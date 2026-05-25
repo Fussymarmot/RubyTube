7c16b56ef2194b01a7f25b50343b7c403a6d101c
Fussymarmot <dimafofanov87@gmail.com>
2026-05-25 20:08:57 +0300

    spelling error correction + settings file update

--- RubyTube/settings.py
+++ RubyTube/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-h2h@sx*+=r=#j^pjytaui0c1jw6&krdvpd9a0!nnvu636dgo0i
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'users',
+    'videos'
 ]
 
 MIDDLEWARE = [
@@ -102,7 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/6.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-RU'
 
 TIME_ZONE = 'UTC'
 
