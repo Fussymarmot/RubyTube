af4fe6a67279ea848ffc74d6f864905296aaa392
Fussymarmot <dimafofanov87@gmail.com>
2026-05-25 20:32:54 +0300

    create users model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,12 @@
-from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class User(AbstractUser):
+
+    class Meta:
+        verbose_name = 'user'
+        verbose_name_plural = 'users'
+        db_table = 'users'
+
+    def __str__(self):
+        return self.username
